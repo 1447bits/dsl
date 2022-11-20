fd651a803147a0d9a95940dfaf4bbef13aa0df68
Atharv Vyas <95745167+Mr-okey@users.noreply.github.com>
2022-11-20 23:07:18 +0530

    Update Group C (19).cpp

--- Group C (19).cpp
+++ Group C (19).cpp
@@ -1,219 +1,398 @@
-#include<iostream>
-#include<string.h>
+/*
+
+ASSIGNMENT 6.
+Department of Computer Engineering has student's club named 'Pinnacle Club'.
+Students of Second, third and final year of department can be granted membership
+on request. Similarly one may cancel the membership of club. First node is reserved
+for president of club and last node is reserved for secretary of club. Write C++
+program to maintain club member‘s information using singly linked list. Store
+student PRN and Name. Write functions to
+a) Add and delete the members as well as president or even secretary.
+b) Compute total number of members of club
+c) Display members
+d) Display list in reverse order using recursion
+e) Two linked lists exists for two divisions. Concatenate two lists.
+
+*/
+
+#include <iostream>
+#include <string.h>
 using namespace std;
- struct node
-{    int prn,rollno;
-      char name[50];
-      struct node *next;
+
+//Node
+struct node {
+	int prn;
+	string name;
+	struct node *next;
 };
-class info
-{        node *s=NULL,*head1=NULL,*temp1=NULL,*head2=NULL,*temp2=NULL,*head=NULL,*temp=NULL;
-        int b,c,i,j,ct;
-        char a[20];
-        public:
-    
-            node *create();
-            void insertp();
-            void insertm();
-            void delm();
-            void delp();
-            void dels();
-            void display();
-            void count();
-            void reverse();
-            void rev(node *p);
-            void concat();
-            
-}  ;        
-node *info::create()
-{   node *p=new(struct node);
-     cout<<"enter name of student ";
-     cin>>a;
-     strcpy(p->name,a);
-       cout<<"\n  enter prn no. of student \n";
-     cin>>b;
-     p->prn=b;
-     cout<<"enter student rollno";
-     cin>>c;
-     p->rollno=c;
-     p->next=NULL;
-     return  p;
-  } 
-  void info::insertm()
-  { 
-       node *p=create();
-   
-     if(head==NULL)
-     {    head=p;
-     }
-    else
-    {      temp=head;
-          while(temp->next!=NULL)
-          {    temp=temp->next;   }
-              temp->next=p;
-     }        
-         
-   }
-     void info::insertp()
-  { 
-       node *p=create();
-   
-     if(head==NULL)
-     {    head=p;
-     }
-    else
-    {      temp=head;
-            head=p;
-              head->next=temp->next;
-             
-     }        
-         
-   }
-  
-   void info::display()
-   {          if(head==NULL)
-               {    cout<<"linklist is empty";
-                }
-              else
-              {   
-                temp=head;
-                          cout<<"     prn     rolln0    NAME   \n";
-                          while(temp->next!=NULL)
-                          {     cout<<"    \n"<<temp->prn<<"    "<<temp->rollno<<"    "<<temp->name;
-                                temp=temp->next;
-                          }
-                         cout<<"    "<<temp->prn<<"    "<<temp->rollno<<"    "<<temp->name;
-               }     
-  }
-  void info::delm()
-  {  int m,f=0; 
-     cout<<"\n enter the prn no. of student whose data you want to delete";
-      cin>>m;
-      temp=head;
-      while(temp->next!=NULL)
-      {  
-           if(temp->prn==m)
-            {           s->next=temp->next;
-                         delete(temp);        f=1;
-            }
-            s=temp;
-            temp=temp->next;
-       }      if(f==0)
-             {   cout<<"\n sorry memeber not deleted ";   }
-   }
-   void info::delp()
-  {     temp=head;
-      head=head->next;
-         delete(temp);
-      }
-      void info::dels()
-  {   
-      temp=head;
-      while(temp->next!=NULL)
-      {   s=temp;
-      temp=temp->next;
-      }     s->next=NULL;
-         delete(temp);
-               
-   }
-   void info::count()
-   {      temp=head;    ct=0;
-          while(temp->next!=NULL)
-          {    temp=temp->next; ct++;   }
-             ct++;
-             cout<<"  Count of members is:"<<ct;
-             
-     }
-     void info::reverse()
-     {     rev(head);   }
-     void info::rev(node *temp)
-     {     if(temp==NULL)
-           { return;   }
-           else
-           {   rev(temp->next); }
-           cout<<"    "<<temp->prn<<"    "<<temp->rollno<<"    "<<temp->name;
-     }  
-        
-     void info::concat()
-    {  int k,j;
-       cout<<"enter no. of members in list1";
-       cin>>k;
-        head=NULL;
-       for(i=0;i<k;i++)
-       { insertm();
-         head1=head;
-          
-       } head=NULL;
-      cout<<"enter no. of members in list2";
-       cin>>j;
-       for(i=0;i<j;i++)
-       { insertm();
-         head2=head;
-        
-       } head=NULL;
-            
-        temp1=head1;
-      while(temp1->next!=NULL)
-      {   temp1=temp1->next;   }
-            temp1->next=head2;
-                       
 
-                              temp2=head1;                
-                             cout<<"     prn     rolln0    NAME   \n";
-                          while(temp2->next!=NULL)
-                          { 
-                             cout<<"\n    "<<temp2->prn<<"    "<<temp2->rollno<<"      "<<temp2->name<<"\n";;
-                             temp2=temp2->next;
-                          }
-                             cout<<"\n    "<<temp2->prn<<"    "<<temp2->rollno<<"      "<<temp2->name<<"\n";
-     }   
-  int main()
-  { info s;
-  int i;
-   
-          char ch;
-       do{
-          cout<<"\n choice the options";
-          cout<<"\n  1. To insert president   ";
-          cout<<"\n  2. To insert member   ";
-          cout<<"\n  3. To insert secretary ";
-          cout<<"\n  4. To delete president   ";
-          cout<<"\n  5. To delete member  ";
-          cout<<"\n  6. To delete secretary ";
-          cout<<"\n  7. To display data   ";
-          cout<<"\n  8. Count of members";
-          cout<<"\n  9. To display reverse of string ";
-          cout<<"\n  10.To concatenate two strings ";
-          cin>>i;
-         switch(i)
-         {        case 1:   s.insertp();
-                                  break;
-                  case 2:   s.insertm();
-                                  break;
-                  case 3:   s.insertm();
-                                  break;
-                  case 4:   s.delp();
-                                  break;
-                  case 5:   s.delm();
-                                  break;
-                  case 6:   s.dels();
-                                  break;
-                  case 7:   s.display();   
-                                  break;
-                  case 8:   s.count();
-                                  break;      
-                  case 9:   s.reverse();
-                                  break;         
-                  case 10:  s.concat();
-                                                      
-                                  break;                                  
-                  default:  cout<<"\n unknown choice";
-          }
-            cout<<"\n do you want to continue enter y/Y \n";
-            cin>>ch;
-       
-       }while(ch=='y'||ch=='Y');                                                                                                   
-                     
-   return 0;
- } 
+//Linked List
+class list {
+	node *head, *temp;
+	public:
+		list() {
+			head = NULL;
+		}
+		node *create(int val, string n);
+		void insertEnd();
+		void insertBeg();
+		void deleteAt(int i);
+		void insertAt(int i);
+		void display();
+		int count();
+		void reverse();
+		void rev(node *t);
+		node* readAt(int i);
+		void concatenate(list A,list B);
+		void op();
+};
 
+//Create
+node* list::create(int val, string n) {
+	temp = new(struct node);
+	if (temp == NULL) {
+		cout<<"Memory Allocation Failed!"<<endl;
+		return 0;
+	} else {
+		temp -> prn = val;
+		temp -> name = n;
+		temp -> next = NULL;
+		return temp;
+	}
+}
+
+//Insert End
+void list::insertEnd() {
+	int val;
+	string n;
+	cout<<"Enter PRN: ";
+	cin>>val;
+	cout<<"Enter Name: ";
+	cin>>n;
+	struct node *t = head;
+	temp = create(val,n);
+	if (head == NULL) {
+		head = temp;
+		head -> next = NULL;
+	} else {
+		while ((t -> next) != NULL) {
+			t = t -> next;
+		}
+		temp -> next = NULL;
+		t -> next = temp;
+		cout<<"Element Inserted at Last"<<endl;
+	}
+}
+
+//Insert At
+void list::insertAt(int i) {
+	int val,pos = i - 1,counter = 1;
+	string n;
+	struct node *ptr;
+	struct node *t = head;
+	while ((t -> next) != NULL) {						//loop to count number of items in linked list.
+			t = t -> next;
+			counter++;
+	}
+	t = head;								//traverse pointer is pointed to head again.
+	if (i == 1) {								//equivalent to insert at start.
+		insertBeg();
+	} else if (pos > counter || i <= 0) {					//if position is greater than the actual linked list.
+		cout<<"Entered position is out of scope."<<endl;
+	} else {								//insert at required position.
+		cout<<"Enter PRN: ";
+		cin>>val;
+		cout<<"Enter Name: ";
+		cin>>n;
+		temp = create(val,n);
+		while (pos--) {
+			ptr = t;
+			t = t -> next;
+		}
+		temp -> next = t;
+		ptr -> next = temp;
+		cout<<"Member Inserted at Position: "<<i<<endl;
+	}
+}
+
+//Delete At
+void list::deleteAt(int i) {
+	int val,pos = i - 1,counter = 1;
+	string n;
+	struct node *ptrl,*ptrr;
+	struct node *t = head;
+	while ((t -> next) != NULL) {
+			t = t -> next;
+			counter++;
+	}
+	t = head;
+	if (i == 1) {
+		ptrl = head;
+		head = head -> next;
+		delete ptrl;
+	} else if (pos > counter || i <= 0) {
+		cout<<"Entered member doesn't exist."<<endl;
+	} else {
+		while (pos--) {
+			ptrl = t;
+			t = t -> next;
+			ptrr = t -> next;
+		}
+		ptrl -> next = ptrr;
+		delete t;
+		cout<<"Member Deleted at Position: "<<i<<endl;
+	}
+}
+
+//Insert Beg
+void list::insertBeg() {
+	int val;
+	string n;
+	cout<<"Enter PRN: ";
+	cin>>val;
+	cout<<"Enter Name: ";
+	cin>>n;
+	//v = val;
+	struct node *t = head;
+	temp = create(val,n);
+	if (head == NULL) {
+		head = temp;
+		head -> next = NULL;
+	} else {
+		temp -> next = head;
+		head = temp;
+		cout<<"We have a New President."<<endl;
+	}
+}
+
+//Display
+void list::display() {
+	temp = head;
+	cout<<"President: ";
+	cout<< temp -> prn<<" — "<<temp -> name<<" -> ";
+	if(temp -> next != NULL) {
+		temp = temp -> next;
+	}
+	while (temp -> next != NULL) {
+		cout<< temp -> prn<<" — "<<temp -> name<<" -> ";
+		temp = temp -> next;
+	}
+	cout<<"Secretary: ";
+	cout<< temp -> prn<<" — "<<temp -> name<<" -> ";
+	cout<<"NULL"<<endl;
+}
+
+//Count
+int list::count() {
+	temp = head;
+	int ct = 0;
+	while (temp != NULL) {
+		ct++;
+		temp = temp -> next;
+	}
+	return ct;
+}
+
+//Concatenate
+void list::concatenate(list A,list B) {
+	struct node * last,*last1;
+	node* t = A.head;
+	while (t != NULL) {
+		int val = t -> prn;
+		string n = t -> name;
+		temp = create(val,n);
+		if (head == NULL) {
+			head = temp;
+			head -> next = NULL;
+			last=head;
+		} else {
+			//temp -> next = NULL;
+			last -> next = t;
+			last=t;
+		}
+		t = t -> next;
+	}
+	last -> next = B.head;
+	t = B.head;
+	while (t != NULL) {
+		int val = t -> prn;
+		string n = t -> name;
+		temp = create(val,n);
+
+			last -> next = temp;
+			last= temp;
+
+		t = t -> next;
+	}
+	last->next=NULL;
+}
+//Accept
+void list::op() {
+	while(1) {
+		int choice;
+		cout<<"\nEnter: \n1. Add \n2. Delete \n3. Member's Count \n4. Display \n5. Reverse the List \n0. Prev Menu"<<endl;
+		cin>>choice;
+		switch(choice) {
+			case 1: { //Add
+				char c;
+				cout<<"\nEnter: \nA. Add President \nB. Add Secretary \nC. Add Member"<<endl;
+				cin>>c;
+				switch(c) {
+					case 'A':
+					case 'a':{
+						insertBeg();
+						break;
+					}
+					case 'B':
+					case 'b': {
+						insertEnd();
+						break;
+					}
+					case 'C':
+					case 'c': {
+						insertAt(2);
+						break;
+					}
+				}
+				break;
+			}
+			case 2: { //Delete
+				char c;
+				cout<<"\nEnter: \nA. Delete President \nB. Delete Secretary \nC. Delete Member"<<endl;
+				cin>>c;
+				switch(c) {
+					case 'A': {
+						deleteAt(1);
+						cout<<"Club must have a President. Enter Details"<<endl;
+						insertBeg();
+						break;
+					}
+					case 'B': {
+						deleteAt(count());
+						cout<<"Club must have a Secretary. Enter Details"<<endl;
+						insertEnd();
+						break;
+					}
+					case 'C': {
+						int j;
+						cout<<"Enter Position for Deletion"<<endl;
+						cin>>j;
+						deleteAt(j);
+						break;
+					}
+				}
+				break;
+			}
+			case 3: { //Count
+				cout<<"Count: "<<count()<<endl;
+				break;
+			}
+			case 4: { //Display
+				if (head == NULL) {
+					cout<<"NULL"<<endl;
+					break;
+				} else {
+					display();
+					break;
+				}
+
+			}
+			case 5: { //Reverse
+				reverse();
+				break;
+			}
+			case 0: { //Prev Menu
+				return;
+			}
+		}
+	}
+}
+
+//Reverse Recursion
+void list::rev(node *t) {
+	if(t -> next != NULL) {
+		rev (t -> next);
+	}
+	if(t == head)
+		cout<<"Secretary: "<<t -> prn<<" — "<<t -> name<<endl;
+	else if(t -> next == NULL)
+		cout<<"President: "<<t -> prn<<" — "<<t -> name<<" -> ";
+	else
+		cout<<"Member: "<<t -> prn<<" — "<<t -> name<<" -> ";
+}
+
+//Reverse
+void list::reverse() {
+	rev(head);
+}
+
+//Read At
+node* list::readAt(int i) {
+	struct node *t = head;
+	int c = count();
+	while(c--) {
+		t = t-> next;
+	}
+	return t;
+}
+
+//Main
+int main() {
+	list L,X,Y;
+	int c;
+	while(1) {
+		cout<<"Enter: \n1. List A \n2. List B \n3. Concatenate\n0. Exit"<<endl;
+		cin>>c;
+		switch(c) {
+			case 1: cout<<"\nList A:"; X.op(); break;
+			case 2: cout<<"\nList B:"; Y.op(); break;
+			case 3: L.concatenate(X,Y); L.display(); break;
+			case 0: return 0;
+		}
+	}
+}
+
+//OUTPUT
+
+/*
+//Main Menu
+Enter:
+1. List A
+2. List B
+3. Concatenate
+0. Exit
+
+//Secondary Menu
+Enter:
+1. Add
+2. Delete
+3. Member's Count
+4. Display
+5. Reverse the List
+0. Prev Menu
+
+//1. Add
+Enter:
+A. Delete President
+B. Delete Secretary
+C. Delete Member
+
+//2. Delete
+Enter:
+A. Delete President
+B. Delete Secretary
+C. Delete Member
+
+//3. Count
+Count : 7
+
+//4. Display
+President: 1 — A -> 2 — B -> Secretary: 3 — C -> NULL
+
+//5. Reverse
+President: 3 — C -> 2 — B -> Secretary: 1 — A -> NULL
+
+//Concatenate
+President: 1 — A -> 2 — B -> 3 — C -> 01 — a -> 02 — b -> Secretary: 03 — c -> NULL
+
+*/
